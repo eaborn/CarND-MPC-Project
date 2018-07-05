0407f6e6b3848750022b01e544fbf860dc70c293
eaborn <yibo211@gmail.com>
2018-07-05 11:26:45 +0200

    add latency of 100ms

--- src/main.cpp
+++ src/main.cpp
@@ -91,6 +91,8 @@ int main() {
           double py = j[1]["y"];
           double psi = j[1]["psi"];
           double v = j[1]["speed"];
+          double delta= j[1]["steering_angle"];
+          double a = j[1]["throttle"];
 
           /*
           * TODO: Calculate steering angle and throttle using MPC.
@@ -122,14 +124,36 @@ int main() {
           Eigen::Map<Eigen::VectorXd> waypoints_y_eig(ptry, 6);
 
           auto coeffs = polyfit(waypoints_x_eig, waypoints_y_eig, 3);
-          double cte = polyeval(coeffs, 0);  // px = 0, py = 0
-          double epsi = -atan(coeffs[1]);  // p
+
+          // Actuator delay in milliseconds.
+          int actuatorDelay =  100;
+
+          // Actuator delay in seconds.
+          double delay = actuatorDelay / 1000.0;
+
+          // Initial state.
+          double x0 = 0;
+          double y0 = 0;
+          double psi0 = 0;
+          double cte0 = coeffs[0];
+          double epsi0 = -atan(coeffs[1]);
+
+          // State after delay.
+          double x_delay = x0 + (v * cos(psi0) * delay);
+          double y_delay = y0 + (v * sin(psi0) * delay);
+          double psi_delay = psi0 - (v * delta * delay / mpc.Lf);
+          double v_delay = v + a * delay;
+          double cte_delay = cte0 + (v * sin(epsi0) * delay);
+          double epsi_delay = epsi0 - (v * atan(coeffs[1]) * delay / mpc.Lf);
+
+          // Define the state vector.
+          Eigen::VectorXd state(6);
+          state << x_delay, y_delay, psi_delay, v_delay, cte_delay, epsi_delay;
+
 
           double steer_value = j[1]["steering_angle"];
           double throttle_value = j[1]["throttle"];
 
-          Eigen::VectorXd state(6);
-          state << 0, 0, 0, v, cte, epsi;
           auto vars = mpc.Solve(state, coeffs);
           steer_value = vars[0];
           throttle_value = vars[1];
